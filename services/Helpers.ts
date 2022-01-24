50281080b839df889fcdb7d202bfa47c890aed24
Raphael Bernhart <mail@raphaelbernhart.at>
2022-01-24 21:12:41 +0100

    ✨ Add helpers file

---
+++ services/Helpers.ts
@@ -0,0 +1,16 @@
+export const genID = (length: number) => {
+    const id = () => {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    let i = 0;
+    let finalID = id();
+
+    while (i < length-1) {
+        finalID = finalID + '-' + id();
+
+        i++;
+    }
+
+    return finalID;
+}
