0edbc88e679c6c7c80241f793395b017651880ee
Raphael Bernhart <mail@raphaelbernhart.at>
2022-01-24 21:15:22 +0100

    ✨ Add Services Link

---
+++ components/widgets/ServicesLink.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3 class="text-8xl font-display cursor-pointer underline underline-offset-4 hover:no-underline transition-all duration-1000">{{ text }}</h3>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'ServiceLinkWidget',
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
