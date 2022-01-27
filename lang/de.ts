16ab0545720c9e26fe86ed7e4529b61e74e1ae71
Raphael Bernhart <mail@raphaelbernhart.at>
2022-01-27 09:02:27 +0100

    🚧 Add lang records for development

--- lang/de.ts
+++ lang/de.ts
@@ -1,7 +1,14 @@
 export default {
     home: {
         head: {
-            lead: 'Creative and dev from austria'
+            lead: 'Creative and Dev from austria',
+            marking: 'Das ist ein Beispiel Text, um zu zeigen, was hier stehen könnte. Das hier könnte eine Produktbeschreibung oder eine Projektbeschreibung sein.'
+        },
+        'section-1': {
+            intro: 'Ich arbeite mit Unternehmen und Agenturen unterschiedlichster Form und Größe zusammen, um <b>Webdesigns & Filmarbeiten</b> zu erstellen, die einen bleibenden Eindruck hinterlassen. Ich bin immer auf der Suche nach neuen und spannenden Projekten, also <b>lassen Sie uns zusammenarbeiten.</b>'
         }
+    },
+    about: {
+        test: 'Das ist die about seite'
     }
 }
--- lang/en.ts
+++ lang/en.ts
@@ -1,7 +1,14 @@
 export default {
     home: {
         head: {
-            lead: 'Creative and Dev from Austria'
+            lead: 'Creative and Dev from Austria',
+            marking: 'This is a sample text to show what could be here. This could be a product description or a project description.'
+        },
+        'section-1': {
+            intro: "I work with brands and agencies of all different shapes and sizes to create web designs and films that leave a lasting impression. I'm always looking to get my teeth stuck into new and exciting projects, so let's collaborate!."
         }
+    },
+    about: {
+        test: 'This is the about site'
     }
 }
