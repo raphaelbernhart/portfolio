64e387694c22d7393348c66b26457fc441766761
Raphael Bernhart <mail@raphaelbernhart.at>
2022-01-27 08:59:02 +0100

    🎨 Improve Paragraph component

--- components/global/ParagraphComponent.vue
+++ components/global/ParagraphComponent.vue
@@ -1,10 +1,22 @@
-<template>
-    <p class="leading-7">
-        <slot></slot>
-    </p>
-</template>
-
 <script lang="ts">
+/* eslint-disable vue/no-v-html */
+
 import Vue from 'vue'
-export default Vue.extend({})
+import { sanitize } from '@/services/Helpers'
+
+export default Vue.extend({
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        sanitize
+    }
+})
 </script>
+
+<template>
+    <p class="leading-7" v-html="sanitize(text)" ></p>
+</template>
