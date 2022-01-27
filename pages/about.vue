6fdc8fa13e35e6ad1dbee8e1c6cedc1330198e6f
Raphael Bernhart <mail@raphaelbernhart.at>
2022-01-27 09:01:22 +0100

    ✨ Add about page

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{ $t('about.test') }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        name: 'AboutPage',
+        head() {
+            return {
+                title: 'About'
+            }
+        }
+    })
+</script>
