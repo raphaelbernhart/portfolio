376e825d2ee39634b5d2baea53e41606f8a860db
Raphael Bernhart <mail@raphaelbernhart.at>
2022-01-28 15:49:01 +0100

    ✨ Add sections on homepage

--- lang/de.ts
+++ lang/de.ts
@@ -6,6 +6,26 @@ export default {
         },
         'section-1': {
             intro: 'Ich arbeite mit Unternehmen und Agenturen unterschiedlichster Form und Größe zusammen, um <b>Webdesigns & Filmarbeiten</b> zu erstellen, die einen bleibenden Eindruck hinterlassen. Ich bin immer auf der Suche nach neuen und spannenden Projekten, also <b>lassen Sie uns zusammenarbeiten.</b>'
+        },
+        'section-3': {
+            headline: 'Welche Leistungen <br>ich anbiete',
+            services: {
+                1: {
+                    title: 'Web Entwicklung',
+                    text: 'Binden Sie Nutzer mit einzigartigen digitalen Erlebnissen'
+                },
+                2: {
+                    title: 'Filmproduktion',
+                    text: 'Erzählen Sie mit hochwertigem Film große Geschichten'
+                },
+                3: {
+                    title: 'Branding',
+                    text: 'Erstellen Sie einzigartige digitale und analoge Auftritte'
+                }
+            }
+        },
+        'section-4': {
+            headline: 'Ready for take-off?'
         }
     },
     about: {
--- lang/en.ts
+++ lang/en.ts
@@ -6,7 +6,24 @@ export default {
         },
         'section-1': {
             intro: "I work with brands and agencies of all different shapes and sizes to create web designs and films that leave a lasting impression. I'm always looking to get my teeth stuck into new and exciting projects, so let's collaborate!."
-        }
+        },
+        'section-3': {
+            headline: 'What services <br>I offer',
+            services: {
+                1: {
+                    title: 'Web Development',
+                    text: 'Engage users with unique Digital Experiences'
+                },
+                2: {
+                    title: 'Production',
+                    text: 'Tell great stories with high-quality film'
+                },
+                3: {
+                    title: 'Branding',
+                    text: 'Create unique digital and analog Appearances'
+                }
+            }
+        },
     },
     about: {
         test: 'This is the about site'
