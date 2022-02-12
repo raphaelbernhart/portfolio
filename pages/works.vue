3a1895c13c8612e2404e9007befd9123758c0f4b
Raphael Bernhart <mail@raphaelbernhart.at>
2022-02-12 17:46:17 +0100

    ✨ Create works page

---
+++ pages/works.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="h-screen container flex items-center">
+        <h1 class="text-4xl">Works Page</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'WorksPage',
+    head() {
+        return {
+            title: 'Works',
+        };
+    },
+});
+</script>
