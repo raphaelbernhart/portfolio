a7558e5da89e53002c9f0c79bb71865fce9cfe8a
Raphael Bernhart <mail@raphaelbernhart.at>
2022-02-14 09:33:19 +0100

    🚚 Change routing for works

---
+++ pages/works/index.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="relative h-screen bg-primary">
+        <!-- <div class="pl-48">
+            <div class="flex justify-between">
+                <div class="flex items-center gap-x-4">
+                    <span class="text-white text-xl font-light">01</span>
+                    <div class="h-[1px] w-28 bg-white"></div>
+                    <span class="text-white text-xl font-light">14</span>
+                </div>
+                <div
+                    class="flex items-center gap-x-2 text-white cursor-pointer"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                    >
+                        <title>Filter Icon</title>
+                        <g class="fill-current">
+                            <path
+                                d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"
+                            ></path>
+                        </g>
+                    </svg>
+                    <span class="text-white text-xl font-light uppercase"
+                        >Filter</span
+                    >
+                </div>
+            </div>
+            <div class="relative flex flex-nowrap">
+                <div class="relative inline-block max-h-[650px]">
+                    <div
+                        class="text-right inline-block absolute bottom-16 left-0"
+                    >
+                        <h2 class="text-9xl font-display text-white">
+                            Syndena
+                        </h2>
+                        <h3 class="text-xl uppercase text-white">
+                            Branding, Design
+                        </h3>
+                    </div>
+                    <img class="bg-gray-600 w-[1100px] h-[650px] ml-36" />
+                </div>
+                <div class="relative inline-block max-h-[650px]">
+                    <div
+                        class="text-right inline-block absolute bottom-16 left-0"
+                    >
+                        <h2 class="text-9xl font-display text-white">
+                            Syndena
+                        </h2>
+                        <h3 class="text-xl uppercase text-white">
+                            Branding, Design
+                        </h3>
+                    </div>
+                    <img class="bg-gray-600 w-[1100px] h-[650px] ml-36" />
+                </div>
+                <div class="relative inline-block max-h-[650px]">
+                    <div
+                        class="text-right inline-block absolute bottom-16 left-0"
+                    >
+                        <h2 class="text-9xl font-display text-white">
+                            Syndena
+                        </h2>
+                        <h3 class="text-xl uppercase text-white">
+                            Branding, Design
+                        </h3>
+                    </div>
+                    <img class="bg-gray-600 w-[1100px] h-[650px] ml-36" />
+                </div>
+                <div class="relative inline-block max-h-[650px]">
+                    <div
+                        class="text-right inline-block absolute bottom-16 left-0"
+                    >
+                        <h2 class="text-9xl font-display text-white">
+                            Syndena
+                        </h2>
+                        <h3 class="text-xl uppercase text-white">
+                            Branding, Design
+                        </h3>
+                    </div>
+                    <img class="bg-gray-600 w-[1100px] h-[650px] ml-36" />
+                </div>
+            </div>
+        </div> -->
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'WorksPage',
+    layout: 'horizontal',
+    head() {
+        return {
+            title: 'Works',
+        };
+    },
+});
+</script>
