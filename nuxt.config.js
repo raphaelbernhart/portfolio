d42e97d8ce09d9c161b6a19b829cadf4e4c0baf6
Raphael Bernhart <mail@raphaelbernhart.at>
2022-04-20 19:44:58 +0200

    🎨 Prettify nuxtjs config code :)

--- nuxt.config.js
+++ nuxt.config.js
@@ -148,10 +148,8 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL:
-            process.env.NODE_ENV === 'development'
-                ? 'http://localhost:3000'
-                : 'https://raphaelbernhart.at',
+        baseURL: '/',
+        browserBaseURL: '/',
     },
 
     // PWA module configuration: https://go.nuxtjs.dev/pwa
