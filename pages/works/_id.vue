5b8a39fc55816bfe2f62ba86cdc32c2fbd0a61e6
raphaelbernhart <mail@raphaelbernhart.at>
2022-05-09 23:38:31 +0200

    🐛 Fix image carousel bug on project page

--- pages/works/_id.vue
+++ pages/works/_id.vue
@@ -148,7 +148,7 @@
                         <div class="w-full h-full inline-block">
                             <img
                                 class="w-full object-cover max-h-[650px]"
-                                :src="project.image[0]"
+                                :src="`https://content.raphaelbernhart.at/assets/${project.imageCarousel[0].directus_files_id}?width=960&height=650&quality=90&format=webp`"
                                 alt=""
                             />
                         </div>
@@ -161,7 +161,7 @@
                         <div class="w-full inline-block">
                             <img
                                 class="w-full h-full object-cover max-h-[350px]"
-                                :src="project.image[1]"
+                                :src="`https://content.raphaelbernhart.at/assets/${project.imageCarousel[1].directus_files_id}?width=500&height=300&quality=90&format=webp`"
                                 alt=""
                             />
                         </div>
@@ -236,7 +236,7 @@ export default Vue.extend({
 
         // Get Project
         const res = await $axios.get(
-            `${process.env.CONTENT_API_URL}items/rb_portfolio_projects/${id}`,
+            `${process.env.CONTENT_API_URL}items/rb_portfolio_projects/${id}?fields=*,imageCarousel.*`,
         );
 
         const project = res.data.data;
